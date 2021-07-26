064864a6cf8b5acb7c56a55e1855224ffdfd2536
John Quarmor <johnequarmor@gmail.com>
2021-07-26 03:26:03 +0000

    Added "Object Restructuring" revision file

---
+++ 02_intermediate/07_objects II/02_object_restructuring.js
@@ -0,0 +1,10 @@
+full_name = "Samantha Quinn";
+age_2 = 44;
+gender_2 = "Female";
+married_2 = true;
+
+// Object literal enhancement/Restructuring an object:
+person_2 = { full_name, age_2, gender_2, married_2 };
+
+console.log(person_2.full_name);
+console.log(person_2.gender_2);
