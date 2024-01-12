c18ee70a8742c75acd5367b542e05db66405965b
John Quarmor <johnequarmor@gmail.com>
2024-01-12 16:19:24 +0000

    Corrected variable name, added log outputs. Modified comments.

--- 01_beginner/01_variables_&_datatypes/01_variables.js
+++ 01_beginner/01_variables_&_datatypes/01_variables.js
@@ -1,11 +1,15 @@
-//Declaring a variable (value is "undefined" by default):
+// Declaring a variable (value is "undefined" by default):
 let myVar1;
+console.log(myVar1);
 
 // Defining a variable (assigning a value to an undefined variable):
-myVar = "This variable now has this string value assigned to it.";
+myVar1 = "This variable now has this string value assigned to it.";
+console.log(myVar1);
 
-// Initializing a variable (assigning a value immediately after creating a variable):
+// Initializing a variable (assigning a value as the variable is created):
 let myVar2 = "I am the string initial value of this variable.";
+console.log(myVar2);
 
 // Reassigning/redefining a variable's value:
 myVar2 = "I am the new string value of this variable.";
+console.log(myVar2);
