1c033f5c8fe374ee2bcfd47a125d59712794daca
John Quarmor <johnequarmor@gmail.com>
2024-01-13 20:33:03 +0000

    Edited revision file.

--- 01_beginner/03_objects/04_object_create_1.js
+++ 01_beginner/03_objects/04_object_create_1.js
@@ -1,20 +1,22 @@
+// Defining a prototype for book objects, this prototype will be passed into the Object.create() method when creating a new object to allow the new object inherit its methods.
 const bookPrototypes = {
 	getBookSummary: function () {
-		return `${this.title} was written by ${this.author} and was first published in ${this.year}`;
+		return `${this.title} was written by ${this.author} and was first published in ${this.year}.`;
 	},
 	getBookAge: function () {
-		let year = new Date().getFullYear;
-		let age = year - this.year;
-		return `${this.name} is ${age} years old`;
+		let currentYear = new Date().getFullYear();
+		let currentAge = currentYear - this.year;
+		return `The book ${this.title} is ${currentAge} years old.`;
 	},
 };
 
-// Creating a new object using Object.create() function:
-// The "bookPrototypes" variable is passed as an argument of the Object.create() method.
+// Creating a new object using Object.create() method passing the object prototype variable as an argument to allow created object to inherit `bookPrototypes` methods:
 const book1 = Object.create(bookPrototypes);
-book1.title = "BlindSight";
-book1.author = "Peter Watts";
-book1.year = 2010;
+book1.title = "BlindSight"; // Assigning value to `title` key.
+book1.author = "Peter Watts"; // Assigning value to `author` key.
+book1.year = 2010; // Assigning value to `year` key.
 
+// Logging to console:
 console.log(book1);
 console.log(book1.getBookSummary());
+console.log(book1.getBookAge());
