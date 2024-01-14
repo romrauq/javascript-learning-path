ef43256cea6a63823c9c19751645d8a138aa55fc
John Quarmor <johnequarmor@gmail.com>
2024-01-14 11:47:35 +0000

    Edited revision file.

--- 01_beginner/03_objects/08_subclasses.js
+++ 01_beginner/03_objects/08_subclasses.js
@@ -1,4 +1,4 @@
-// Main (super) class:
+// Defining a main (super) class:
 class Book {
 	constructor(name, author, year) {
 		this.name = name;
@@ -6,19 +6,20 @@ class Book {
 		this.year = year;
 	}
 
+	// Defining class method named `getSummary`:
 	getSummary() {
 		return `${this.name} was written by ${this.author} and was first published in ${this.year}`;
 	}
 }
 
-// Creating a new subclass "Comic" of the main "Book" class:
+// Defining a (sub)class of the main `Book` class named `Comic`:
 class Comic extends Book {
 	constructor(name, author, year, artist) {
-		super(name, author, year); // Adds the existing properties of the (super) "Book" class
+		super(name, author, year); // Adds existing properties of the (super) "Book" class
 		this.artist = artist; // defines the value assignment of the additional "artist" property of the "Comic" subclass.
 	}
 }
 
-let comic1 = new Comic("Jupiter's Legacy", "Mark Millar", 2015, "Lou Banderas");
-console.log(comic1);
-console.log(comic1.getSummary()); // The method of the super class automatically works for the subclass.
+let comic = new Comic("Jupiter's Legacy", "Mark Millar", 2015, "Lou Banderas");
+console.log(comic);
+console.log(comic.getSummary()); // The method of the super class automatically works for the subclass.
