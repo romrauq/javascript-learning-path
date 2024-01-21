87f8165e8195b933bff9a0ec7f0b626c671a9788
John Quarmor <johnequarmor@gmail.com>
2024-01-21 13:04:18 +0000

    Edited revision file.

--- 01_beginner/08_high_order_array_methods/02_filter.js
+++ 01_beginner/08_high_order_array_methods/02_filter.js
@@ -12,23 +12,25 @@ const studentAges = [
 	{ name: "Kano", age: 28 },
 ];
 
-// .filter() method usage:
+// .filter() method with regular function usage:
 const youth = studentAges.filter(function (student) {
 	if (student.age <= 25) {
 		return true;
 	}
 });
 
-console.log(youth); // returns an array containing filtered objects.
+console.log("Under 25:", youth, "\n"); // returns an array containing filtered objects.
 
+// .filter() method with arrow function usage:
 const middleAged = studentAges.filter((students) => {
 	if (students.age > 25 && students.age <= 60) {
 		return true;
 	}
 });
 
-console.log(middleAged); // returns an array containing filtered objects.
+console.log("Over 25 & Under 60:", middleAged, "\n"); // returns an array containing filtered objects.
 
+// .filter() method with a more concise arrow function usage:
 const elderly = studentAges.filter((student) => student.age >= 60);
 
 console.log(elderly); // returns an array containing filtered objects.
